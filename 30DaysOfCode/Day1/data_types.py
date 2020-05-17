2b747b30ecf15d810cec6b8773c9eb7011109fb3
Chandan Verma <verma.chandan01@gmail.com>
2020-05-17 10:39:29 +0530

    30 days of code
    Day 1

---
+++ 30DaysOfCode/Day1/data_types.py
@@ -0,0 +1,24 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a = 5
+b = 7.0
+c = 'Rocks'
+# Read and save an integer, double, and String to your variables.
+
+a = int(input())
+b = float(input())
+c = str(input())
+# Print the sum of both integer variables on a new line.
+
+int_sum = i + a
+print(int_sum)
+# Print the sum of the double variables on a new line.
+
+float_sum = d + b
+print(float_sum)
+# Concatenate and print the String variables on a new line
+str_sum = s + c
+print(str_sum)
+# The 's' variable above should be printed first.
